4b903b03dfeb438b249f8d98bce7340d5db8382d
quadang21cen <quadang21cen@gmail.com>
2025-05-29 02:13:00 +0700

    test: update cicd

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!' # Thay đổi ở đây
+    return 'Hello, CI/CD with GitHub Actions!'
 
 if __name__ == '__main__':
     app.run(debug=True)
